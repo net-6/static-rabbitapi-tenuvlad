06eabdd57409d834949e6d076e1065b8896bf9d0
Tenu Vlad <tenuvlad@yahoo.com>
2020-02-15 11:58:55 +0200

    Rabbit

---
+++ ApiRabbit/Models/Rabbit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRabbit.Models
+{
+    public class Rabbit
+    {
+        public enum FurColors
+        {
+            White,
+            Brown,
+            Black,
+            Grey
+        }
+        public enum EyeColors
+        {
+            Blue,
+            Black,
+            Red
+        }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [Required]
+        public FurColors FurColor { get; set; }
+        [Required]
+        public EyeColors EyeColor { get; set; }
+        [Required]
+        public Gender Genders { get; set; }
+    }
+}
